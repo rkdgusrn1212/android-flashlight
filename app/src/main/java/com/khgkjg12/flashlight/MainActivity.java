579f357a9c58af63a57ad309d3b9a006f484dac7
Kang Hyungu <khgkjg12@naver.com>
2018-08-30 16:19:41 +0900

    중간 커밋, 메인 멕티비티 작업중, 카메라 선언 부분과 스위치 이벤트 콜백 완성

--- app/src/main/java/com/khgkjg12/flashlight/MainActivity.java
+++ app/src/main/java/com/khgkjg12/flashlight/MainActivity.java
@@ -1,7 +1,15 @@
 package com.khgkjg12.flashlight;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Switch flashSwitch = findViewById(R.id.flash_switch);
+
+        if(checkCameraHardware(this)){
+            Camera camera = null;
+            try {
+                camera = Camera.open(); // attempt to get a Camera instance
+            }
+            catch (Exception e){
+                onNoCamera(this);
+            }
+            if(camera!=null){
+                final Camera finalCamera = camera;
+                flashSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        if(isChecked){
+                            finalCamera.getParameters().setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+                        }else{
+                            finalCamera.getParameters().setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                        }
+                    }
+                });
+            }else {
+                onNoCamera(this);
+            }
+        }else{
+            onNoCamera(this);
+        }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    private void onNoCamera(Context context){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.no_camera).setNeutralButton(R.string.confirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
     }
 }
